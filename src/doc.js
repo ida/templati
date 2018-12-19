caa19cc8a0da0c12cfc164d6cf19e22d392d346e
Ida Ebkes <contact@ida-ebkes.eu>
2018-12-19 10:08:46 +0100

    Improve doc.js

--- src/doc.js
+++ src/doc.js
@@ -1,42 +1,54 @@
 const Tag = require('./tag').Tag
 
 
+function ini(doc) {
+
+  if(doc.filePath.endsWith('.html')) { // remove extension
+    doc.filePath = doc.filePath.slice(0, 5)
+  }
+
+  let name = doc.filePath.split('/') // get everything after last slash
+  name = name[name.length-1]
+  name = name.split('.')[0]
+  name = name[0].toUpperCase() + name.slice(1) // uppercase name
+
+
+  let tag = doc.addTag('html', { lang: 'en-gb'})
+
+  tag = tag.addTag('head')
+
+  tag.addTag('title', {}, name)
+
+  tag.addTag('meta', { charset: 'utf-8'})
+
+  tag = doc.content[0]
+
+  for(let i in doc.stylePaths) {
+    doc.addStyleSheet(doc.scriptPaths[i])
+  }
+
+  for(let i in doc.scriptPaths) {
+    doc.addScriptFile( doc.scriptPaths[i])
+  }
+
+  return tag.addTag('body')
+
+}
+
+
+
 class Doc extends Tag {
 
-  constructor(renderPath='untitled', stylePaths=[], scriptPaths=[]) {
+  constructor(filePath='untitled', stylePaths=[], scriptPaths=[]) {
 
     super('!doctype', { html: true })
 
-    this.name = renderPath.split('/')
-    this.name = this.name[this.name.length-1]
-    this.name = this.name.split('.')[0]
-    this.renderPath = renderPath
+    this.filePath = filePath
     this.stylePaths = stylePaths
     this.scriptPaths = scriptPaths
+    this.body = ini(this)
 
-
-    let tag = this.addTag('html', { lang: 'en-gb'})
-
-		    tag = tag.addTag('head')
-
-          tag.addTag('title', {}, this.name[0].toUpperCase() + this.name.slice(1) )
-
-          tag.addTag('meta', { charset: 'utf-8'})
-
-        tag = this.content[0]
-
-		    this.body = tag.addTag('body')
-
-
-		for(let i in this.stylePaths) {
-			this.addStyleSheet( this.scriptPaths[i] + '.css' )
-		}
-
-		for(let i in this.scriptPaths) {
-			this.addScriptFile( this.scriptPaths[i] + '.js' )
-		}
-
-	}
+  }
 
 
   addScriptFile(path) {
@@ -45,18 +57,18 @@ class Doc extends Tag {
 
 
   addStyleSheet(path) {
-    this.content[0].content[0].addTag('link', { href: path,
-                                        type: 'text/css',
-                                        rel: 'stylesheet'
-    })
+    this.content[0].content[0].addTag(
+      'link',
+      { href: path, type: 'text/css', rel: 'stylesheet'}
+    );
   }
 
   writeConfig() {
-    this.writeJson(this.renderPath)
+    this.writeJson(this.filePath + '.json')
   }
 
   writeDoc() {
-    this.writeHtml(this.renderPath)
+    this.writeHtml(this.filePath + '.html')
   }
 
 }
